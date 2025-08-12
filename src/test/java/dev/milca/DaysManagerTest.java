18bfadf889e86ec8a6329717c6e7a81cabb21034
milcaponce <milcaponce.dev@gmail.com>
2025-08-13 00:32:26 +0200

    test: Añadir el último test para el método clearList

--- src/test/java/dev/milca/DaysManagerTest.java
+++ src/test/java/dev/milca/DaysManagerTest.java
@@ -76,7 +76,7 @@ public class DaysManagerTest {
         DaysManager daysManager = new DaysManager();
         daysManager.createListOfDays();
         boolean result = daysManager.existsDay("Mábado");
-        Assertions.assertFalse(result, "El método debe devolver false si el día Mábado no existe.");
+        Assertions.assertFalse(result, "El método debe devolver false si el día Mábado no existe");
     }
 
     @Test
@@ -86,7 +86,15 @@ public class DaysManagerTest {
         daysManager.orderList();
         List<String> orderedList = daysManager.returnDays();
         Assertions.assertEquals("Domingo", orderedList.get(0), "El primer día de la lista ordenada deber ser Domingo.");
-        Assertions.assertEquals("Jueves", orderedList.get(1), "El segundo día de la lista ordenada debe ser Jueves.");
+        Assertions.assertEquals("Jueves", orderedList.get(1), "El segundo día de la lista ordenada debe ser Jueves");
+    }
+
+    @Test
+    void testClearListShouldRemoveAllDays() {
+        DaysManager daysManager = new DaysManager();
+        daysManager.createListOfDays();
+        daysManager.clearList();
+        Assertions.assertEquals(0, daysManager.getListSize(), "El tamaño de la lista debe ser 0 después de vaciarla");
     }
 }
 
