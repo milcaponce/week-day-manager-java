5f919d3d174933a70309708406069e92720800e0
milcaponce <milcaponce.dev@gmail.com>
2025-08-12 22:37:08 +0200

    test: Añadir test para createListOfDays

--- src/test/java/dev/milca/DaysManagerTest.java
+++ src/test/java/dev/milca/DaysManagerTest.java
@@ -9,4 +9,18 @@ public class DaysManagerTest {
         int listSize = daysManager.getListSize();
         Assertions.assertEquals(0, listSize);
     }
+
+    @Test
+    void testCreateListOfDaysShouldAddSevenDaysToTheList() {
+        DaysManager daysManager = new DaysManager();
+        daysManager.createListOfDays();
+        Assertions.assertEquals(7, daysManager.getListSize());
+    }
 }
+
+
+/*Teoría para el test:
+ * Arrange: crear instancia de la clase para que el estado inicial del objeto sea el esperado. (DaysManager daysManager = new DaysManager();)
+ * Act: llamar al método que se quiere probar (daysManager.createListOfDays();)
+ * Assert: Verificar el resultado, se llama al método y se utiliza Assertions.assertEquals... para comparar el resultado con el valor esperado
+ */
